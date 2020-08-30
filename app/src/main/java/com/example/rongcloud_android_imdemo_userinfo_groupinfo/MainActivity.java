dd70ca6924d042907df071b70d530ec19c9c0ec3
guozongkui <guozongkui@rongcloud.cn>
2020-08-30 17:47:31 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/rongcloud_android_imdemo_userinfo_groupinfo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rongcloud_android_imdemo_userinfo_groupinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
